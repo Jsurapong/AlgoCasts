e3427498aa839b1868fb65af4f7083d922a3cd16
Surapong Kawkangploo <surapong.kawkangploo@gmail.com>
2020-03-20 10:39:30 +0700

    [palidrome]

--- palindrome/index.js
+++ palindrome/index.js
@@ -7,6 +7,15 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+// function palindrome(str) {
+//   const reversed = str.split("").reduce((rev, char) => `${char}${rev}`, "");
 
+//   return reversed === str;
+// }
+
+function palindrome(str) {
+  return str.split("").every((char, i) => {
+    return char === str[str.length - i - 1];
+  });
+}
 module.exports = palindrome;
