043bb97ed394998f0f171372c310fddd0df3971e
Surapong Kawkangploo <surapong.kawkangploo@gmail.com>
2020-03-20 11:34:55 +0700

    [maxchar]

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,6 +5,23 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {};
+  let max = 0;
+  let charMax = "";
+
+  for (let char of str) {
+    charMap[char] = charMap[char] ? charMap[char] + 1 : 1;
+  }
+
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      charMax = char;
+    }
+  }
+
+  return charMax;
+}
 
 module.exports = maxChar;
