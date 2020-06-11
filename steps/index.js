0e1b2d93b1eab7502e73470421b7b1ae6af10ee3
Surapong Kawkangploo <surapong.kawkangploo@gmail.com>
2020-06-11 15:18:00 +0700

    [steps]

--- steps/index.js
+++ steps/index.js
@@ -17,6 +17,39 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let stair = "";
+
+//     for (let column = 0; column < n; column++) {
+//       if (column <= row) {
+//         stair += "#";
+//       } else {
+//         stair += " ";
+//       }
+//     }
+
+//     console.log(stair);
+//   }
+// }
+
+function steps(n, row = 0, stair = "") {
+  if (n === row) {
+    return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  if (stair.length <= row) {
+    stair += "#";
+  } else {
+    stair += " ";
+  }
+
+  steps(n, row, stair);
+}
 
 module.exports = steps;
